f22251e9e4cbe43be3c53950f8b9a705fce7ae2a
Liang Xu <755674130@qq.com>
2017-03-30 22:00:13 +0800

    修改歌词解析逻辑

--- html/js/lrc_scroll.js
+++ html/js/lrc_scroll.js
@@ -4,23 +4,42 @@ var lrcScroll;
     var lrcBox = document.getElementById('lrc_box');//歌词框
     lrcScroll = function (lrc) {
         //处理数据
-        var lrcArray = lrc.replace(/\[\D(.*)]/, '').replace(/\[\d*:\d*((\.|\:)\d*)*\]/g, '++').split('++');
-        lrcArray.shift();
-        var timeArray = lrc.match(/\[\d*:\d*((\.|\:)\d*)*\]/g);
-        for (var i = 0; i < timeArray.length; i++) {
-            timeArray[i] = Number(timeArray[i].slice(1, 3)) * 60 + Number(timeArray[i].slice(4, 9));
+        var lrcArray = lrc.split('\n');
+        var dataObj = {},
+            strArray = [],
+            timeArray = [];
+        for (var m = 0; m < lrcArray.length; m++) {
+            var clause = lrcArray[m].replace(/\[\d*:\d*((\.|\:)\d*)*\]/g, '');
+            var timeRegExpArr = lrcArray[m].match(/\[\d*:\d*((\.|\:)\d*)*\]/g);
+            if (timeRegExpArr) {
+                for (var k = 0; k < timeRegExpArr.length; k++) {
+                    var time = Number(timeRegExpArr[k].slice(1, 3)) * 60 + Number(timeRegExpArr[k].slice(4, 9));
+                    dataObj[time] = clause;
+                }
+            }
+        }
+        for (var key in dataObj) {
+            timeArray.push(key);
+        }
+        var length = Object.getOwnPropertyNames(dataObj).length;
+        timeArray.sort(function (a, b) {
+            return a - b;
+        })
+        for (var n = 0; n < length; n++) {
+            strArray.push(dataObj[timeArray[n]]);
         }
         //插入DOM
         var ul = document.createElement('ul');
-        for (var j = 0; j < lrcArray.length; j++) {
+        for (var l = 0; l < strArray.length; l++) {
             var item = document.createElement('li');
-            item.innerHTML = lrcArray[j];
+            item.innerHTML = strArray[l];
             ul.appendChild(item);
         }
+        lrcBox.innerHTML = '';
         lrcBox.appendChild(ul);
         //歌词滚动
         ul.style.transition = 'transform 0.2s ease-out';
-        ul.style.transform = 'translate3d(0px, ' + lrcBox.offsetHeight * 2 / 5 + 'px, 0px)';
+        ul.style.transform = 'translate3d(0px, ' + lrcBox.offsetHeight * 1 / 5 + 'px, 0px)';
         var lrc_timer = setInterval(function () {
             for (var i = 0; i < timeArray.length; i++) {
                 if (Math.abs(audio.currentTime - timeArray[i]) < 0.1) {
@@ -32,10 +51,10 @@ var lrcScroll;
                         ul.children[o].className = '';
                     }
                     ul.children[i].className = 'lrc_on';
-                    ul.style.transform = 'translate3d(0px, -' + (sumOffsetHight - lrcBox.offsetHeight * 2 / 5) + 'px, 0px)';
+                    ul.style.transform = 'translate3d(0px, -' + (sumOffsetHight - lrcBox.offsetHeight * 1 / 6) + 'px, 0px)';
                 }
             }
         }, 50)
-
     }
-})()
+}
+)()
