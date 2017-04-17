95e33b3e8068508d6b05c146f3e6aba437a1dad9
Liang Xu <755674130@qq.com>
2017-04-17 23:15:50 +0800

    change: drag init

--- html/js/lrc_scroll.js
+++ html/js/lrc_scroll.js
@@ -61,9 +61,9 @@ var lrcScroll;
         var ul = lrcBox.firstElementChild,
             flag = false,
             startY = 0,
-            offset = 0;
+            offset = lrcBox.offsetHeight * 2 / 5;
         ul.style.transition = 'transform 0.2s ease-out';
-        ul.style.transform = 'translate3d(0px, ' + lrcBox.offsetHeight * 2 / 5 + 'px, 0px)';
+        ul.style.transform = 'translate3d(0px, ' + offset + 'px, 0px)';
         var lrc_timer = setInterval(function () {
             for (var i = 0; i < timeArray.length; i++) {
                 if (Math.abs(audio.currentTime - timeArray[i]) < 0.1) {
